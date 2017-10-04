005e142106bd0edb99839306542abc37e0c5f8d5
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-04 16:56:03 -0700

    0.1.0

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'cas_magento'
+maintainer 'Cascadia Cookbooks'
+maintainer_email 'null'
+license 'GPL-3.0'
+description 'Prepares an environment for Magento 2'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
+source_url 'https://github.com/cascadia-cookbooks/magento'
+issues_url 'https://github.com/cascadia-cookbooks/magento/issues'
+
+supports 'ubuntu', '>= 16.04'
+supports 'rhel', '>= 7'
+supports 'centos', '>= 7'
+supports 'debian', '>= 8'
