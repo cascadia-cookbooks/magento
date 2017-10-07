9ad140726660709f1a3c6b8bcc68556c0f5c8363
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-07 08:20:52 -0700

    Update with defined attributes

--- recipes/directories.rb
+++ recipes/directories.rb
@@ -4,21 +4,25 @@
 # Description:: Creates and maintains directory structure
 #
 
+owner = node['cas_magento']['directories']['owner']
+group = node['cas_magento']['directories']['group']
+mode  = node['cas_magento']['directories']['mode']
+
 # Manage docroot
-directory node['cas_magento']['docroot'] do
-    owner     pass
-    group     pass
-    mode      pass
+directory "#{node['cas_magento']['directories']['docroot']}" do
+    owner     owner
+    group     group
+    mode      mode
     recursive true
     action    :create
 end
 
 # Manage default directory structure
 node['cas_magento']['directories']['default'].each do |dir|
-    directory "#{node['cas_magento']['docroot']}/#{dir}" do
-        owner     pass
-        group     pass
-        mode      pass
+    directory "#{node['cas_magento']['directories']['docroot']}/#{dir}" do
+        owner     owner
+        group     group
+        mode      mode
         recursive true
         action    :create
     end
@@ -26,10 +30,10 @@ end
 
 # Manage any user specified domains
 node['cas_magento']['directories']['additional'].each do |dir|
-    directory "#{node['cas_magento']['docroot']}/#{dir}" do
-        owner     pass
-        group     pass
-        mode      pass
+    directory "#{node['cas_magento']['directories']['docroot']}/#{dir}" do
+        owner     owner
+        group     group
+        mode      mode
         recursive true
         action    :create
     end
@@ -39,9 +43,9 @@ end
 if node.chef_environment == 'development'
     node['cas_magento']['directories']['vagrant'].each do |dir|
         directory dir do
-            owner     pass
-            group     pass
-            mode      pass
+            owner     owner
+            group     group
+            mode      mode
             recursive true
             action    :create
         end
